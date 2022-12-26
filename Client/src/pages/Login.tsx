39557676908a8d20508dec90988974a0098d2da4
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2022-12-26 16:21:13 +0530

    login page half built

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from "react";
+import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
+import "./css/Login.css"; // import background image CSS file
+import { BsArrowRightCircle } from "react-icons/bs";
 
 const Login: React.FC = () => {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <Container
+      className="login-container"
+      style={{
+        backgroundImage: `url(/img/login-back.png)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      {" "}
+      {/* add a container to hold the content and apply the background image */}
+      <Row className="justify-content-center">
+        {" "}
+        {/* center the content horizontally */}
+        <Col lg={8}>
+          <Image src={"/img/login-left.png"} className="login-left" />
+          <h1>Efficient Asset Management Equals Success</h1>
+        </Col>
+        <Col lg={4}>
+          {" "}
+          {/* set the width of the column to take up 7 out of 12 grid columns */}
+          <Form className="login-form">
+            <Image src={"/img/logo.png"} className="logo" />
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="submit-btn">
+              Login <BsArrowRightCircle style={{
+                fontSize: "2.2rem",
+                marginRight: "-2rem",
+                marginLeft: "2rem",
+              }}/>
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
