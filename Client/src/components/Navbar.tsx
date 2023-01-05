e4aa95098856b4648a7eb53ee4e00c7c88c43a94
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2023-01-05 19:29:17 +0530

    test

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { Navbar } from 'react-bootstrap';
 const MyNavbar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg">
+      <h1>Krishantha</h1>
       <Navbar.Brand href="#">Enterprise Asset Management</Navbar.Brand>
     </Navbar>
   );
