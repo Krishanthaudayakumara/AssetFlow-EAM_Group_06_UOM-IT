734ba4c263e429b6f55598296af29bf3bac00484
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2023-01-06 18:39:56 +0530

    updated

--- Client/src/components/Sidebar.tsx
+++ Client/src/components/Sidebar.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "react-bootstrap";
-<<<<<<< HEAD
-import { FaHome, FaInfoCircle, FaCogs, FaUser } from "react-icons/fa";
-=======
 import { FaThLarge, FaBox, FaBuilding, FaFileAlt, FaUsers, FaShippingFast, FaComments, FaCalendarAlt } from "react-icons/fa";
->>>>>>> dev
 
 import "../css/Sidebar.css"; // import background image CSS file
 
@@ -32,12 +28,6 @@ import "../css/Sidebar.css"; // import background image CSS file
 // };
 
 const items = [
-<<<<<<< HEAD
-  { label: "Home", link: "/", icon: FaHome },
-  { label: "About", link: "/about", icon: FaInfoCircle },
-  { label: "Settings", link: "/settings", icon: FaCogs },
-  { label: "Profile", link: "/profile", icon: FaUser },
-=======
   { label: "Dashboard", link: "/", icon: FaThLarge },
   { label: "Inventory", link: "/inventory", icon: FaBox },
   { label: "Facility", link: "/facility", icon: FaBuilding },
@@ -52,18 +42,13 @@ const items = [
 
 
   
->>>>>>> dev
 ];
 
 const Sidebar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg" className="sidebar">
       <div className="sidebar-logo">
-<<<<<<< HEAD
-        <img src="/img/logo.png" alt="Logo" width="300" />
-=======
         <img className="logo-img" src="/img/favicon.png" alt="Logo" width="100" />
->>>>>>> dev
       </div>
       <div className="sidebar-nav-padding"></div>
 
@@ -73,11 +58,7 @@ const Sidebar: React.FC = () => {
         {items.map((item) => (
           <NavItem key={item.link}>
             <NavLink className="side-link" href={item.link}>
-<<<<<<< HEAD
-              <item.icon className="side-item mr-2" />
-=======
               <item.icon fontSize="1.2em" className="side-item mr-2" />
->>>>>>> dev
               {item.label}
             </NavLink>
           </NavItem>
