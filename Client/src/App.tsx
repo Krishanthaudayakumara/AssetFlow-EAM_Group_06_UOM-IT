415fde78e64ab0a5454b37fa45aec42c1ce8fda0
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2023-01-06 19:07:14 +0530

    btn

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Facility from "./pages/Facility";
 
+
 const App: React.FC = () => {
   return (
     <Router>
--- Client/src/pages/facility.tsx
+++ Client/src/pages/facility.tsx
@@ -6,7 +6,7 @@ const Facility: React.FC = () => {
     return(
         <div>
      
-            <Button variant="outlined">Import Facilities</Button>
+            <Button variant="primary">Import Facilities</Button>
             <Button variant="outlined">export to excel</Button>
             <Button variant="contained">+ New Building</Button>
             <Button variant="contained">+new Floor</Button>
