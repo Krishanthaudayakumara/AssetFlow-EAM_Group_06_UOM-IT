94404b123a35ecefc501bf1de132301680f1942d
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-01-08 20:11:15 +0530

    Hardcorded  data of Agents

---
+++ Client/src/components/Support/Agents.tsx
@@ -0,0 +1,20 @@
+const Agents = [
+    {
+        id:"1",
+        name:"Vidath Chamikara",
+        age:"23",
+        d:"1",
+        ame:"Vidath Chamikara",
+        ge:"23"
+    },{
+        id:"2",
+        name:"Kamal Perera",
+        age:"25"
+    },{
+        id:"3",
+        name:"Nimal Perera",
+        age:"28"
+    }
+]
+
+export default Agents;
