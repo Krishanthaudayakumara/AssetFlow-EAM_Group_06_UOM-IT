d2f14e7ee20defe4b5522c438085edf833d0c100
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-01-08 20:13:06 +0530

    Update App.tsx file

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Support_Agent from "./pages/Support/Support_Agent";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path = '/Support_Agent' element = {<Support_Agent/>}/>
       </Routes>
     </Router>
   );
