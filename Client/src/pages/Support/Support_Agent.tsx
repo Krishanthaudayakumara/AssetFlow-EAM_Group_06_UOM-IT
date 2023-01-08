73061cf21990f0d608ef2772fc8789b90fd79d7c
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-01-08 20:12:45 +0530

    Create Agent Page

---
+++ Client/src/pages/Support/Support_Agent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Agent_Table from '../../components/Support/Agent_Table';
+
+const Support_Agent: React.FC = () => {
+  return (
+    <div>
+        <Agent_Table/>
+    </div>
+  );
+};
+
+export default Support_Agent;
