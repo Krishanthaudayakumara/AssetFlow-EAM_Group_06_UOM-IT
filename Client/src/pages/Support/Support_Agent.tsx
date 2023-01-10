d29e351e043671647a93a61db894238852354752
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-01-10 09:45:30 +0530

    Update Agent Page

--- Client/src/pages/Support/Support_Agent.tsx
+++ Client/src/pages/Support/Support_Agent.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Agent_Table from '../../components/Support/Agent_Table';
 
+
 const Support_Agent: React.FC = () => {
   return (
+    
     <div>
         <Agent_Table/>
     </div>
