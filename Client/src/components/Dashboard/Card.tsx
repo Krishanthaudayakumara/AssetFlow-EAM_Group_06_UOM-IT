4f7f4ec9532a2ba5ce86a1cfd5c234de79d06f80
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-11 20:26:11 +0530

    Change backgroundColors of cards

--- Client/src/components/Dashboard/Card.tsx
+++ Client/src/components/Dashboard/Card.tsx
@@ -6,8 +6,8 @@ const Card: React.FC = () => {
     <>
     <div className="row mb-3">
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card bg-success text-white h-100">
-            <div className="card-body bg-success" style={{backgroundColor:"#57b960"}}>
+        <div className="card  text-white h-100">
+            <div className="card-body " style={{backgroundColor:"#482890"}}>
                 <div className="rotate">
                     <i className="fa fa-user fa-4x"></i>
                 </div>
@@ -17,8 +17,8 @@ const Card: React.FC = () => {
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white bg-danger h-100">
-            <div className="card-body bg-danger">
+        <div className="card text-white  h-100">
+            <div className="card-body " style={{backgroundColor:"#482890"}}>
                 <div className="rotate">
                     <i className="fa fa-list fa-4x"></i>
                 </div>
@@ -28,8 +28,8 @@ const Card: React.FC = () => {
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white bg-info h-100">
-            <div className="card-body bg-info">
+        <div className="card text-white  h-100">
+            <div className="card-body "style={{backgroundColor:"#482890"}}>
                 <div className="rotate">
                   <i className="fab fa-twitter fa-4x"></i>
                 </div>
@@ -39,8 +39,8 @@ const Card: React.FC = () => {
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white bg-warning h-100">
-            <div className="card-body">
+        <div className="card text-white  h-100">
+            <div className="card-body"style={{backgroundColor:"#482890"}}>
                 <div className="rotate">
                     <i className="fa fa-share fa-4x"></i>
                 </div>
