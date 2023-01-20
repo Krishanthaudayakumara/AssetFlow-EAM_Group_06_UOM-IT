70da35fc9f391557584440875c63c4451683cb58
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-01-20 05:41:30 +0530

    Create the add new agent modal

---
+++ Client/src/components/Support/AddAgent.tsx
@@ -0,0 +1,70 @@
+import { Button, Form } from "react-bootstrap";
+
+const AddAgent = () => {
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Control 
+                      type="text"
+                      placeholder="Name *"
+                      required
+                />
+            </Form.Group><br/>
+
+            <Form.Group>
+                <Form.Control 
+                      type="text"
+                      placeholder="Position *"
+                      required
+                />
+            </Form.Group><br/>
+
+            <Form.Group>
+                <Form.Control 
+                      type="text"
+                      placeholder="Department *"
+                      required
+                />
+            </Form.Group><br/>
+
+            <Form.Group>
+                <Form.Control 
+                      type="email"
+                      placeholder="Email *"
+                      required
+                />
+                           
+            </Form.Group><br/>
+
+            <Form.Group>
+                <Form.Control 
+                      type="number"
+                      placeholder="Pending"
+                      required
+                />
+                               
+            </Form.Group><br/>
+
+            <Form.Group>
+                <Form.Control 
+                      type="number"
+                      placeholder="Completed"
+                      required
+                />
+                               
+            </Form.Group><br/>  
+
+            <Form.Group>
+            <Form.Select aria-label="Default select example">
+                
+                <option value="1">Not Available</option>
+                <option value="2">Available</option>
+            </Form.Select>
+            </Form.Group><br/>
+
+            <Button variant="success" type="submit">Submit</Button>
+        </Form>
+    )
+}
+
+export default AddAgent;
