f2a53bcec7912990d519ddd201ae07371ad54c3f
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-22 14:01:56 +0530

    Align ReportFilter

--- Client/src/components/Report/ReportFilter.tsx
+++ Client/src/components/Report/ReportFilter.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
+import { Form, Row, Col } from "react-bootstrap";
 
-const ReportFilter: React.FC = () => {
+
+const ReportFilter: React.FC = () =>{
   return (
     
     <Form >
-  
       <Row>
         <Col xs><Dropdown className="d-inline mx-2">
         <Dropdown.Toggle id="dropdown-autoclose-true">
@@ -20,39 +20,27 @@ const ReportFilter: React.FC = () => {
         </Dropdown.Menu>
         </Dropdown>
         </Col>
-
-        <Col xs={{ order: 12 }}>
-        <Dropdown className="d-inline mx-2" autoClose="inside">
-      <Dropdown.Toggle id="dropdown-autoclose-inside">
-       From date
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#">dd Item</Dropdown.Item>
-        <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-
-        </Col>
-        <Col xs={{ order: 1 }}>
-
-        <Dropdown className="d-inline mx-2" autoClose="outside">
-      <Dropdown.Toggle id="dropdown-autoclose-outside">
-       To Date
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-
-    
-    </Col>
-      </Row>
-      
+        <Col xs={{ order:12}}>
+        <div className="row">
+                    <div className="col-md-8"style={{margin:"5px 100px 0 0"}}>
+                        <Form.Group controlId="dob">
+                            <Form.Label>From Date</Form.Label>
+                            <Form.Control type="date" name="dob" placeholder="Date of Birth" />
+                        </Form.Group>
+                    </div>
+                    </div>
+          </Col> 
+          <Col xs={{ order:12}}>
+        <div className="row">
+                    <div className="col-md-8"style={{margin:"5px 100px 0 0"}}>
+                        <Form.Group controlId="dob">
+                            <Form.Label>To Date</Form.Label>
+                            <Form.Control type="date" name="dob" placeholder="Date of Birth" />
+                        </Form.Group>
+                    </div>
+                    </div>
+          </Col> 
+          </Row>   
 </Form>
 
   ); 
