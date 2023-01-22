eaf0613eafdce5543efdd973c4ad1a816e76db43
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-22 14:03:13 +0530

    updated report

--- Client/src/pages/Report.tsx
+++ Client/src/pages/Report.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Container, Row, Col, Nav, Table } from "react-bootstrap";
-import { BsArrowRightCircle } from "react-icons/bs";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import ReportFilter from "../components/Report/ReportFilter";
+import ITTable from "../components/Report/ITTable";
 import "../css/Home.css"; 
+import ReportButton from '../components/Report/ReportButton';
 
-/*interface Props {
+
+interface Props {
     onFilter: (department: string, fromDate: Date, toDate: Date) => void;
   }
-  */const data = [
-  { id: "1", User: "Krish", UserName: "@mark", Role: "krish",Department: "Krish", Email: "Krish",JoinedDate: "Krish",Actions: "Krish"},
-  { id: "2", User: "Vidath",UserName : "@Sam",Role : "krish",Department: "Krish", Email: "Krish",JoinedDate: "Krish",Actions: "Krish" },
-  { id: "3", User: "Chamudi",UserName : "@jane", Role: "krish",Department: "Krish", Email: "Krish",JoinedDate: "Krish",Actions: "Krish" },
-  { id: "1", User: "Krish",UserName : "@mark",Role : "krish",Department: "Krish", Email: "Krish",JoinedDate: "Krish",Actions: "Krish" },
-];
-const Report:  React.FC = () =>{
+
+  /*const BarChart: FC<Props> = (props: any) => {
  
-    /*const [department, setDepartment] = useState('');
+    const [department, setDepartment] = useState('');
     const [fromDate, setFromDate] = useState(new Date());
     const [toDate, setToDate] = useState(new Date());
   
@@ -25,10 +22,14 @@ const Report:  React.FC = () =>{
       event.preventDefault();
       onFilter(department, fromDate, toDate);
     };
+    export default Report;*/
  
  
- 
- */
+
+ /*   function onFilter(department: string, fromDate: Date, toDate: Date) {
+      throw new Error('Function not implemented.');
+    }*/
+ const Report:  React.FC = () =>{
   return (
     <Container>
     <Row>
@@ -41,41 +42,19 @@ const Report:  React.FC = () =>{
       <Col md={9}>
         <div>
           <Navbar />
-          <h1>Report</h1>
-          
-          {/*<ReportFilter/>*/}
-          
-
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-              <th>id</th>
-              <th>User</th>
-              <th>User Name</th>
-              <th>Role</th>
-              <th>Department</th>
-              <th>Email</th>
-              <th>Joined date</th>
-              <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr>
-                  <td>{item.id}</td>
-                  <td>{item.User}</td>
-                  <td>{item.UserName}</td>
-                   <td>{item.Role}</td>
-                  <td>{item.Department}</td>
-                  <td>{item.Email}</td>
-                  <td>{item.JoinedDate}</td>
-                  <td>{item.Actions}</td>
-                  
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
+          <h1 style={{margin:"0px 0 0 65px"}}>Report</h1>
+          <div className="shadow p-3 mb-5 bg-white rounded"style={{margin:"0px 0 0 65px"}} >
+          <ReportFilter/>
+         
+          </div>
+          <ReportButton/>
+          <div style={{margin:"0px 0 0 65px"}}> 
+            <ITTable/>
+            </div>
+            
+           
+          </div>
+       
       </Col>
     </Row>
   </Container>
