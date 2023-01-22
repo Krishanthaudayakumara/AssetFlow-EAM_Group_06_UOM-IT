8b9d0aee02680d820c5945b99450ace72f30ddbd
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-22 14:00:25 +0530

    Update icons

--- Client/src/components/Dashboard/Card.tsx
+++ Client/src/components/Dashboard/Card.tsx
@@ -1,41 +1,58 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBox, faTicket, faUser, faWarehouse } from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Card: React.FC = () => {
   return (
     <>
-    <div className="row mb-3">
+    <div className="row mb-3" >
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card  text-white h-100">
-            <div className="card-body " style={{backgroundColor:"#482890"}}>
-                
+        <div className="card  text-white h-40 "style={{borderRadius:"10px"}}>
+        
+            <div className="card-body " style={{backgroundColor:"#482890",borderRadius:"10px"}}>
+            <div className="rotate">
+                <FontAwesomeIcon icon={faUser} />
+                </div>
+               
                 <h6 className="text-uppercase">Available Users</h6>
                 <h1 className="display-4">134</h1>
             </div>
+            
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white  h-100">
-            <div className="card-body " style={{backgroundColor:"#482890"}}>
-                
+        <div className="card text-white  h-40"style={{borderRadius:"10px"}}>
+            <div className="card-body " style={{backgroundColor:"#482890",borderRadius:"10px"}}>
+            <div className="rotate">
+                <FontAwesomeIcon icon={faBox} />
+                </div>
+               
                 <h6 className="text-uppercase">Total Inventory</h6>
                 <h1 className="display-4">87</h1>
             </div>
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white  h-100">
-            <div className="card-body "style={{backgroundColor:"#482890"}}>
-                
-                <h6 className="text-uppercase">Assigned Assets</h6>
+        <div className="card text-white  h-100"style={{borderRadius:"10px"}}>
+            <div className="card-body "style={{backgroundColor:"#482890",borderRadius:"10px"}}>
+            <div className="rotate">
+                <FontAwesomeIcon icon={faWarehouse} />
+                </div>
+               
+                <h6 className="text-uppercase">Assigned Assets </h6>
                 <h1 className="display-4">125</h1>
             </div>
         </div>
     </div>
     <div className="col-xl-3 col-sm-6 py-2">
-        <div className="card text-white  h-100">
-            <div className="card-body"style={{backgroundColor:"#482890"}}>
-                
+        <div className="card text-white  h-100"style={{borderRadius:"10px"}}>
+            <div className="card-body"style={{backgroundColor:"#482890",borderRadius:"10px"}}>
+            <div className="rotate">
+                <FontAwesomeIcon icon={faTicket} />
+                </div>
+               
                 <h6 className="text-uppercase">Support Tickets</h6>
                 <h1 className="display-4">36</h1>
             </div>
