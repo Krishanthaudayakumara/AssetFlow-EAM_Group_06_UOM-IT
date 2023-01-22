c6a032e08c6f3f19024bcd6d846dfd8235452e8c
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-22 14:05:55 +0530

    Create ReportButton

---
+++ Client/src/components/Report/ReportButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const ReportButton: React.FC = () => {
+    return (
+      <div>
+       <Button className="btn btn-outline light btn" style={{margin:"20px 0 0 665px"}}>Genarated Report</Button>
+       
+      </div>
+    );
+  };
+  
+  export default ReportButton;
