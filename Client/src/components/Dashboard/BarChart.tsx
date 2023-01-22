a270563b8ac3f0f1d4f9031dd44d42ae3e8c1794
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-01-22 14:03:43 +0530

    Add props

--- Client/src/components/Dashboard/BarChart.tsx
+++ Client/src/components/Dashboard/BarChart.tsx
@@ -1,59 +1,28 @@
-import React from 'react';
-import {Chart as ChartJs,BarElement,CategoryScale,LinearScale,Tooltip,Legend} from "chart.js/auto";
-import { Bar} from 'react-chartjs-2';
-
-ChartJs.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Tooltip,
-    Legend
-  );
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top' as const,
-      },
-      title: {
-        display: true,
-       
-      },
-    },
-  };
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July','Augut'];
-  const data = {
-    labels,
+import React, { FC } from 'react';
+import { Bar } from 'react-chartjs-2';
+interface BarChartProps {
+  data: {
+    labels: string[];
     datasets: [
       {
-        label: 'Issued',
-        data: [12, 19, 3, 5, 2, 3,1,5],
-        backgroundColor: "#482890",
+        label: string;
+        data: number[];
+        backgroundColor: string;
       },
       {
-        label: 'Returned',
-        data: [20, 10, 2, 5, 2, 3,8,9],
-        backgroundColor: "#ff615a",
-      },
-    ],
-  };
-
-const BarChart: React.FC = () => {
-     return (
-      <>
-     <div   style={
-      {padding: "20px",
-      width:"90%"
+        label: string;
+        data: number[];
+        backgroundColor: string;
       }
-}>
-    <Bar
-      data={data} 
-      options={options}  
-      /> ;
-      </div>
-    </>
-  
-    ); 
+    ];
   };
-  
-  export default BarChart;
+  options?: object;
+}
+
+
+
+const BarChart: FC<BarChartProps> = (props) => {
+  return <Bar data={props.data} options={props.options} />;
+};
+
+export default BarChart;
