d54a4b6c4fb10bcc321c6781572a0c6ba531c174
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-02-01 12:28:59 +0530

    Add filters bar chart

--- Client/src/components/Dashboard/BarChart.tsx
+++ Client/src/components/Dashboard/BarChart.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
+
 interface BarChartProps {
   data: {
     labels: string[];
@@ -19,10 +20,79 @@ interface BarChartProps {
   options?: object;
 }
 
-
-
-const BarChart: FC<BarChartProps> = (props) => {
-  return <Bar data={props.data} options={props.options} />;
+const Filter: FC<{ onChange: (value: string) => void }> = ({ onChange }) => {
+  return (
+    <div>
+      <label>
+        <input type="radio" name="filter" value="yearly" onChange={(e) => onChange(e.target.value)} />
+        Yearly
+      </label>
+      <label>
+        <input type="radio" name="filter" value="monthly" onChange={(e) => onChange(e.target.value)} />
+        Monthly
+      </label>
+      <label>
+        <input type="radio" name="filter" value="daily" onChange={(e) => onChange(e.target.value)} />
+        Daily
+      </label>
+    </div>
+  );
 };
+const BarChart: FC<BarChartProps> = (props) => {
+  const [filter, setFilter] = useState('yearly');
+
+  const filteredData = () => {
+    switch (filter) {
+      case 'yearly':
+        return props.data;
+      case 'monthly':
+          const filteredMonthlyData = {
+          labels: props.data.labels.filter((label) => {
+            // Check if the label represents a data for the selected month
+            // For example, you can check if the label starts with the name of the month, etc.
+            return label===('January');
+          }),
+          datasets: props.data.datasets.map((dataset) => {
+            return {
+              label: dataset.label,
+              data: dataset.data.filter((data, index) => {
+                return props.data.labels[index]===('January');
+              }),
+              backgroundColor: dataset.backgroundColor,
+            };
+          }),
+        };
+        return filteredMonthlyData;
+      case 'daily':
+        const filteredDailyData = {
+        labels: props.data.labels.filter((label) => {
+          // Check if the label represents a data for the selected day
+          // For example, you can check if the label is equal to the name of the day, etc.
+          return label === '02/01/2023';
+        }),
+        datasets: props.data.datasets.map((dataset) => {
+          return {
+            label: dataset.label,
+            data: dataset.data.filter((data, index) => {
+              return props.data.labels[index] === '02/01/2023';
+            }),
+            backgroundColor: dataset.backgroundColor,
+          };
+        }),
+      };
+      return filteredDailyData;
+      default:
+        return props.data;
+    }
+  };
+
+  return (
+    <>
+      <Filter onChange={setFilter} />
+      <Bar data={filteredData()} options={props.options} />
+    </>
+  );
+};
+
 
 export default BarChart;
