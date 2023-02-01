e577431e5333e1e1ba1cd1ac54f202bf9fa18d81
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-02-01 12:29:27 +0530

    Add filter linechart

--- Client/src/components/Dashboard/LineChart.tsx
+++ Client/src/components/Dashboard/LineChart.tsx
@@ -1,29 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line} from 'react-chartjs-2';
+import type { ChartData, ChartOptions } from 'chart.js';
 
-interface LineChartProps {
-  data: {
-    labels: string[];
-    datasets: [
-      {
-        label: string;
-        data: number[];
-        borderColor:string;
-        backgroundColor: string;
-      },
-      
-    ];
-  };
-  options?: object;
+
+interface Props {
+  Lineoptions: ChartOptions<'line'>;
+  Linedata: ChartData<'line'>;
 }
+const LineChart: React.FC<Props> = ({ Linedata,Lineoptions }) => {
+    const [filter, setFilter] = useState<'yearly' | 'monthly' | 'daily'>('yearly');
+    const filteredData = () => {
+      if (!Linedata.labels) {
+        return Linedata;
+      }
+      switch (filter) {
+        case 'yearly':
+          // Do not filter the data, return all data
+          return Linedata;
+        case 'monthly':
+          // Filter the data for a particular month
+          const filteredMonthlyData = {
+            labels:  Linedata.labels.filter((label) => {
+              // Check if the label represents a data for the selected month
+              // For example, you can check if the label starts with the name of the month, etc.
+              return label===('February');
+            }),
+            datasets: Linedata.datasets.map((dataset) => {
+              return {
+                label: dataset.label,
+                data: dataset.data.filter((data, index) => {
+                  return Linedata.labels![index]===('February');
+                }),
+                backgroundColor: dataset.backgroundColor,
+              };
+            }),
+          };
+          return filteredMonthlyData;
+        case 'daily':
+          // Filter the data for a particular day
+          const filteredDailyData = {
+            labels: Linedata.labels.filter((label) => {
+              // Check if the label represents a data for the selected day
+              // For example, you can check if the label is equal to the name of the day, etc.
+              return label === '02/01/2023';
+            }),
+            datasets: Linedata.datasets.map((dataset) => {
+              return {
+                label: dataset.label,
+                data: dataset.data.filter((data, index) => {
+                  return Linedata.labels![index] === '02/01/2023';
+                }),
+                backgroundColor: dataset.backgroundColor,
+              };
+            }),
+          };
+          return filteredDailyData;
+        default:
+          return Linedata;
+      }
+    };
 
-const LineChart: React.FC<LineChartProps > = (props) => {
     return (
         <div>
-            <Line data={props.data} options={props.options} />
-        </div>
-    );
-}
+      <select value={filter} onChange={(e) => setFilter(e.target.value as 'yearly' | 'monthly' | 'daily')}>
+        <option value="yearly">Yearly</option>
+        <option value="monthly">Monthly</option>
+        <option value="daily">Daily</option>
+      </select>
+      <Line data={filteredData()} options={Lineoptions} />
+    </div>
+  );
 
+};
 export default LineChart;
-
