5d7f88a9dd00a431c254b63a2546f4429c7f6122
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-02-01 15:48:25 +0530

    Create Ticket Form

---
+++ Client/src/components/Support/NewTicketForm.tsx
@@ -0,0 +1,51 @@
+import { Button, Form } from "react-bootstrap";
+
+const NewTicketForm = () => {
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Control type="text" placeholder="Ticket Name *" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Control type="text" placeholder="Ticket Position *" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Control type="text" placeholder="Department *" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Control type="email" placeholder="Email *" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Control type="number" placeholder="Pending" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Control type="number" placeholder="Completed" required />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Select aria-label="Default select example">
+          <option value="1">Available</option>
+          <option value="2">Not Available</option>
+        </Form.Select>
+      </Form.Group>
+      <br />
+
+      <Button variant="success" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default NewTicketForm;
