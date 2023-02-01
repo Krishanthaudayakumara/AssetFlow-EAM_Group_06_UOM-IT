2457d223dcf42843cb7fb83766749e104ec5643f
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2023-02-01 11:34:49 +0530

    server initial

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Server.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext> (x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-
 
 var app = builder.Build();
 
