c4c61b5fba444dd3070b8927defb87bd9f29b137
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-02-01 12:28:35 +0530

    Update reportbutton

--- Client/src/components/Report/ReportButton.tsx
+++ Client/src/components/Report/ReportButton.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const ReportButton: React.FC = () => {
     return (
       <div>
-       <Button className="btn btn-outline light btn" style={{margin:"20px 0 0 665px"}}>Genarated Report</Button>
+      <div className='container'>
+
+        <div className='row'>
+          <div className='col-9' style={{padding:"0 0 0 450px"}}>
+          
+            <Button  variant="outline-primary"type="button"style={{ backgroundColor: "white" ,borderColor: "#331c7a",color: "#331c7a"}} >Export to Excel</Button>
+          </div>
+          <div className='col-3' style={{padding:"0 0 0 0"}}>
+          
+          <Button  variant="outline-primary"type="button"style={{ backgroundColor: "white" ,borderColor: "#482890",color: "#482890"}} >Generate Report</Button>
+          </div>
+        </div>
+      </div>
        
       </div>
     );
