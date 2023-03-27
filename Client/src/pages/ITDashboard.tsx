8e7f5d28cd07dddc12bccd780135d86cf86b51ac
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-03-27 20:59:15 +0530

    completed

--- Client/src/pages/ITDashboard.tsx
+++ Client/src/pages/ITDashboard.tsx
@@ -1,146 +1,201 @@
-import React from "react";
-import { Container, Row, Col, Table, } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import { Container, Row, Col } from "react-bootstrap";
 import LineChart from "../components/Dashboard/LineChart";
 import "../css/Home.css";
 import CardDashboard from "../components/Dashboard/CardDashboard";
-import BarChart from "../components/Dashboard/BarChart";
 import TicketBarChart from "../components/Dashboard/TicketBarChart";
+interface TicketCount {
+    month: string;
+    count: number;
+  }
+interface TicketSatisfaction {
+    month: string;
+    goodCount: number;
+    betterCount: number;
+    worstCount: number;
+  }
+const ITDashboard = () => {
+  const [ticketCounts, setTicketCounts] = useState<TicketCount[]>([]);
+  const [availableAgentCount, setAvailableAgentCount] = useState<number>(0);
+  const [solvedTicketCount, setSolvedTicketCount] = useState<number>(0);
+  const [averageResponseTime, setAverageResponseTime] = useState<number>(0);
+  const [ticketSatisfaction, setTicketSatisfaction] = useState<TicketSatisfaction[]>([]);
 
-const options = {
-  scales: {
-    x: {
-      title: {
-        display: true,
-        text: 'Month'
-      }
-    },
-    y: {
-      title: {
-        display: true,
-        text: 'Quanty'
-      }
-    }
-  }}
 
-  const Linedata = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  useEffect(() => {
+    axios.get('http://localhost:5293/ITDashboard/monthly-ticket-counts')
+      .then(response => {
+        setTicketCounts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      axios.get('http://localhost:5293/ITDashboard/available-agent-count')
+      .then(response => {
+        setAvailableAgentCount(response.data.availableAgentCount);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      axios
+      .get('http://localhost:5293/ITDashboard/solved-ticket-count')
+      .then((response) => {
+        setSolvedTicketCount(response.data.solvedTicketCount);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      axios
+      .get('http://localhost:5293/ITDashboard/avg-response-time')
+      .then((response) =>{ setAverageResponseTime(response.data);})
+      .catch((error) => {console.log(error)});
+      
+      
+      axios.get('http://localhost:5293/ITDashboard')
+      .then((response: AxiosResponse) => {
+        setTicketSatisfaction(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }, []);
+
+ 
+
+  const lineData = {
+    labels: ticketCounts.map(ticketCount => ticketCount.month),
     datasets: [
       {
-        label: 'Line Chart Data',
-        data: [1200, 1900, 300, 500, 2000, 305, 100],
+        label: 'Ticket Count',
+        data: ticketCounts.map(ticketCount => ticketCount.count),
         borderColor: '#482890',
         backgroundColor: '#482890',
       },
     ],
-  }
+  };
   const Lineoptions = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: {
+          x: 0,
+          y: 0,
+        },
       },
       title: {
         display: true,
-        text: 'Line Chart',
+        text: "Monthly Ticket Count",
       },
     },
     scales: {
       x: {
         title: {
           display: true,
-          text: 'Month',
+          text: "Month",
         },
       },
       y: {
         title: {
           display: true,
-          text: 'Value',
+          text: "Count",
         },
       },
     },
-  }
-const ITDashboard: React.FC = () => {
-  return (
+  };
+  
+  const chartData = {
+    data: {
+      labels: ticketSatisfaction.map((satisfaction) => satisfaction.month),
+      datasets: [
+        {
+          label: 'Good',
+          data: ticketSatisfaction.map((satisfaction) => satisfaction.goodCount),
+          backgroundColor: 'green'
+        },
+        {
+          label: 'Better',
+          data: ticketSatisfaction.map((satisfaction) => satisfaction.betterCount),
+          backgroundColor: 'yellow'
+        },
+        {
+          label: 'Worst',
+          data: ticketSatisfaction.map((satisfaction) => satisfaction.worstCount),
+          backgroundColor: 'red'
+        }
+      ]
+    }
+  };
+  const options = {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Month'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Quanty'
+        }
+      }
+    }};
+return (
     <div>
       <Container>
-             <h4 style={{margin:"0px 0 0 65px"}}>IT Summary</h4>
-                <div className="row mb-6" style={{margin:"0px 0 0 65px"}} >
-                  <CardDashboard name="Available Agents" quantity={98}  />
-                  <CardDashboard  name="Solved Tickets" quantity={98}  />
-                  <CardDashboard  name="Average Response Time" quantity={98}  />
-                  </div>
-            
-            <h4 style={{margin:"0px 0 0 65px"}}
-            >
-            New Tickets
-            </h4>
-            <div>
-              <Row>
-                <Col md={6}>
-                  <div className="shadow p-4 mb-6 bg-white rounded" 
-                    style={{
-                      paddingTop: "100px",
-                      height:"450px",
-                      margin:"0px 0 0 65px",
-                      width: "900px",
-                     alignContent:"center",
-                    }}
-                  >
-                    <LineChart Linedata={Linedata} Lineoptions={Lineoptions} />
-                      
-                      
-                 
-      
-      </div>
-                </Col>
-               
-              </Row>
-            </div>
-            <h4 style={{margin:"0px 0 0 65px"}}>Ticket Satisfaction</h4>
-            <div style={{margin:"0px 0 0 65px"}}>
-              <TicketBarChart data={{
-                  labels: [
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                    'July',
-                  ],
-                  datasets: [
-                    {
-                      label: 'Issued',
-                      data: [40, 20, 30, 50, 90, 10, 20],
-                      backgroundColor: '#482890',
-                    },
-                    {
-                      label: 'Returned',
-                      data: [20, 10, 2, 5, 2, 3, 8, 9],
-                      backgroundColor: '#ff615a',
-                    },
-                    {
-                      label: 'Returned',
-                      data: [20, 10, 2, 5, 2, 3, 8, 9],
-                      backgroundColor: '#ff615a',
-                    },
-                  ],
+        <h4 style={{ margin: "0px 0 0 65px" }}>IT Summary</h4>
+        <div className="row mb-6" style={{ margin: "0px 0 0 65px" }}>
+         
+	                <CardDashboard name="Available Agents" count={availableAgentCount} />
+                  <CardDashboard  name="Solved Tickets" count={solvedTicketCount}  />
+                  <CardDashboard  name="Average Response Time" count={averageResponseTime}  />
+        </div>
+
+        <h4 style={{ margin: "0px 0 0 65px" }}>Monthly Ticket Count</h4>
+        <div>
+          <Row>
+            <Col md={6}>
+              <div
+                className="shadow p-4 mb-6 bg-white rounded"
+                style={{
+                  paddingTop: "100px",
+                  height: "450px",
+                  margin: "0px 0 0 65px",
+                  width: "900px",
+                  alignContent: "center",
                 }}
-                options={options}  />
+              >
+                <LineChart Linedata={lineData} Lineoptions={Lineoptions} />
+              </div>
+            </Col>
+          </Row>
+        </div>
+
+        {/* code for TicketBarChart component */}
+	      <h4 style={{margin:"0px 0 0 65px"}}>Ticket Satisfaction</h4>
+            <div style={{margin:"0px 0 0 65px"}}>
+              <TicketBarChart data={{labels: ticketSatisfaction.map((satisfaction) => satisfaction.month),
+      datasets: [
+      {
+        label: 'Good',
+        data: ticketSatisfaction.map((satisfaction) => satisfaction.goodCount),
+        backgroundColor: 'green'
+      },
+      {
+        label: 'Better',
+        data: ticketSatisfaction.map((satisfaction) => satisfaction.betterCount),
+        backgroundColor: 'yellow'
+      },
+      {
+        label: 'Worst',
+        data: ticketSatisfaction.map((satisfaction) => satisfaction.worstCount),
+        backgroundColor: 'red'
+      }]
+  }} options={options}  />
             </div>
-          
-  
-                  </Container>
-
-
-
+      </Container>
     </div>
-
-  )
-}
-
-
-
-     
-   
-export default ITDashboard ;
+  );
+};
+export default ITDashboard;
