f4dd970be52f6760922d8917a1d15033978bc8c1
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-06-02 15:47:22 +0530

    Update agent view page

--- Client/src/pages/Support/Agents.tsx
+++ Client/src/pages/Support/Agents.tsx
@@ -1,20 +1,50 @@
-import React from "react";
-import { Container, Row, Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import AgentTable from "../../components/Support/Table/AgentTable";
 import NewAgentForm from "../../components/Support/Forms/NewAgentForm";
 
 const Agents: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
   return (
-    <Container>
-      <Row>
-        <div>          
-          <br />
-          <hr style={{ margin: "0 0 0 250px", color: "blue" }} />
-          <AgentTable />
-        </div>
-      </Row>
-      
-    </Container>
+    <div className="container">
+    <div className="row">
+      <div className="col">
+        <p className="page-heading">SUPPORT</p>
+      </div>
+      <div className="col-3" style={{ padding: "0 0 0 70px" }}>
+        <button
+          type="button"
+          className="btn btn-outline-light"
+          onClick={handleShow}
+          style={{ backgroundColor: "#FF615A" }}
+        >
+          + Add New Agent
+        </button>
+      </div>
+    </div>
+    <hr style={{ margin: "0 0 0 300px", color: "blue" }} />
+    <div className="row">
+      <div>
+        <br />
+        <AgentTable />
+      </div>
+    </div>
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header style={{ backgroundColor: "#482890" }}>
+        <Modal.Title style={{ color: "white" }}>New Agent</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <NewAgentForm />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </div>
   );
 };
 
