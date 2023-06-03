d861b6fd9feb322d3eacfe4598b931495f661afb
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-06-03 21:56:39 +0530

    Updated PieChart

--- Client/src/components/Dashboard/PieChart.tsx
+++ Client/src/components/Dashboard/PieChart.tsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import {
-  Chart as ChartJs,
-  Tooltip,
-  Title,
-  ArcElement,
-  Legend,
-} from 'chart.js/auto'
-import { Pie } from 'react-chartjs-2'
-ChartJs.register(Tooltip, Title, ArcElement, Legend)
+import React from 'react';
+import { Chart as ChartJs, Tooltip, Title, ArcElement, Legend } from 'chart.js/auto';
+import { Pie } from 'react-chartjs-2';
+ChartJs.register(Tooltip, Title, ArcElement, Legend);
 
-const data = {
-  labels: ['Table', 'Chair', 'Monitor', 'Mouse'],
-  datasets: [
-    {
-      label: 'Assets Types',
-      data: [1200, 600, 300, 1000],
-      backgroundColor: ['#482890', '#e2a9e5', '#632c65', '#4b384c'],
-      borderColor: ['#482890', '#e2a9e5', '#632c65', '#4b384c'],
-    },
-  ],
+interface PieChartProps {
+  data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor: string[];
+      borderColor: string[];
+    }[];
+  };
 }
-const PieChart: React.FC = () => {
+
+const PieChart: React.FC<PieChartProps> = ({ data }) => {
   return (
     <>
       <div className="class1">
         <Pie data={data} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PieChart
+export default PieChart;
