bd2be801ce5487358be36cb3d0e42505e6a1ec7c
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-06-05 22:29:02 +0530

    Create InventoryBarchart

---
+++ Client/src/components/Dashboard/InventoryBarchart.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+interface ChartData {
+  data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor: string[];
+      barThickness: number;
+    }[];
+  };
+  options?: object;
+}
+
+const InventoryBarchart : FC<ChartData> = (props) => {
+  return <Bar data={props.data} options={props.options} />;
+};
+
+export default InventoryBarchart;
