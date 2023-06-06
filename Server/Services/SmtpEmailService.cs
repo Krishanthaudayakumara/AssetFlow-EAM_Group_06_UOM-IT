7295d0f94ae00228a3e9a1b89783b18ba3cb8c8e
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-06-06 19:15:58 +0530

    Final edit

--- Server/Services/SmtpEmailService.cs
+++ Server/Services/SmtpEmailService.cs
@@ -12,22 +12,22 @@ namespace Server.Services
         {
             _smtpClient = new SmtpClient
             {
-                Host = "smtp.gmail.com", // Your SMTP server
+                Host = "smtp.office365.com", // Your SMTP server
                 Port = 587, // Your SMTP port
-                Credentials = new NetworkCredential("vidathamarasekara99@gmail.com", "ijcjdwrjyuynxiir"),
-                EnableSsl = false
+                Credentials = new NetworkCredential("assetflow.me@outlook.com", "#StingPirates11"),
+                EnableSsl = true
             };
         }
 
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            var mailMessage = new MailMessage("vidathamarasekara99@gmail.com", email, subject, message);
+            var mailMessage = new MailMessage("assetflow.me@outlook.com", email, subject, message);
             await _smtpClient.SendMailAsync(mailMessage);
         }
 
         public async Task SendEmailAsync(string email, string subject, string message, bool isHtml)
         {
-            var mailMessage = new MailMessage("vidathamarasekara99@gmail.com", email, subject, message)
+            var mailMessage = new MailMessage("assetflow.me@outlook.com", email, subject, message)
             {
                 IsBodyHtml = isHtml
             };
