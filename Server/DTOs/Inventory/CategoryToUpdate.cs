36ff013b594f0e2bb7ce0a2b93318ed56322a5a4
Krishantha Kumara <udayakumarakrishantha@gmail.com>
2023-06-07 08:56:15 +0530

    remove inventory

--- Server/DTOs/Inventory/CategoryToUpdate.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.DTOs
-{
-    public class CategoryToUpdate
-    {
-        public string CategoryType { get; set; }
-        public string Description { get; set; }
-        public IFormFile Image { get; set; }
-    }
-}
